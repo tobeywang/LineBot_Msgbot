afa74082878ce047d3ac5ea14ab141039b7bced1
tobeywang0530 <zorowang1018@gmail.com>
2019-09-09 13:20:11 +0800

    push msg

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ bot.on('message', function (event) {
     if (event.message.type = 'text') {
         var msg = event.message.text;
         //重覆使用者說的訊息
-        var replyMsg = `您是說:${event.message.text}`;
+        var replyMsg = `您是說: ${event.message.text}`;
         var token=event.reply.token;
         event.reply(replyMsg+"token:"+token).then(function (data) {
             // success
@@ -28,8 +28,8 @@ bot.on('message', function (event) {
 
 // 主動發送訊息
 setTimeout(function () {
-    var userId = 'Your User ID';
-    var sendMsg = "push hands up ";
+    var userId = 'U7eb1fbc43939ee0e9a81f7d1b337bff8';
+    var sendMsg = "主動推播訊息";
     bot.push(userId, [sendMsg]);
     console.log('userId: ' + userId);
     console.log('send: ' + sendMsg);
