de15ba36bd14dbda83cf697843f3008399927416
tobeywang0530 <zorowang1018@gmail.com>
2023-09-14 15:37:54 +0800

    add docker file

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:12
+ENV NODE_ENV=production
+WORKDIR /app
+COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
+RUN npm install --production --silent && mv node_modules ../
+COPY . .
+RUN chown -R node /app
+EXPOSE 3000
+USER node
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -44,7 +44,12 @@ app.get("/", function (req, res) {
 app.post('/', linebotParser);
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+// var server = app.listen(process.env.PORT || 8080, function () {
+//     var port = server.address().port;
+//     console.log("App now running on port", port);
+// });
+
+app.listen(3000,()=>{   
+    console.log('website is running');
+
 });
